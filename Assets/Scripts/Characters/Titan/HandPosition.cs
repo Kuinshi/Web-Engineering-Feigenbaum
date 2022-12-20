d42fc1d88a69fe96505550f217c59691ac4eaa18
Ken <K.Reisball@googlemail.com>
2022-12-20 22:29:54 +0100

    Improved Physics on th ehand

--- Assets/Scripts/Characters/Titan/HandPosition.cs
+++ Assets/Scripts/Characters/Titan/HandPosition.cs
@@ -6,18 +6,39 @@ namespace Characters.Titan
     {
         [SerializeField] private Rigidbody rb;
         [SerializeField] private Transform followObject;
+        
         [SerializeField] private Vector3 positionOffset;
+        [SerializeField] private float handMovespeed = 777;
+        
         [SerializeField] private Vector3 rotationOffset;
-        [SerializeField] private Vector3 localRotationOffset;
+        
         
 
         private void FixedUpdate()
         {
+            
+            /*
             Vector3 targetPosition = followObject.position + positionOffset;
             rb.MovePosition(targetPosition);
-
+            */
+                        
+            Vector3 targetPosition = followObject.position + positionOffset;
+            float distance = Vector3.Distance(targetPosition, transform.position);
+            Vector3 direction = (targetPosition - transform.position).normalized;
+            rb.velocity = direction * distance * handMovespeed * Time.fixedDeltaTime;
+            
+            
+            
             Quaternion targetRotation = followObject.rotation * Quaternion.Euler(rotationOffset);
             rb.MoveRotation(targetRotation);
+            
+            /*
+            Quaternion targetRotation = followObject.rotation * Quaternion.Euler(rotationOffset);
+            Quaternion rotValue = targetRotation * Quaternion.Inverse(rb.rotation);
+            rotValue.ToAngleAxis(out float angle, out Vector3 axis);
+            rb.angularVelocity = axis * (angle * Mathf.Deg2Rad * handRotspeed * Time.fixedDeltaTime);
+            */
+            
         }
         
     }
