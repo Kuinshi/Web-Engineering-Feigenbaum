c3f847361519a3137fd66954143c159d01691f94
Ken <K.Reisball@googlemail.com>
2023-01-31 14:50:48 +0100

    Incorporated Shop Scene

---
+++ Assets/Scripts/LoadShopScene.cs
@@ -0,0 +1,21 @@
+﻿using UIScripts;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadShopScene : MonoBehaviour
+{
+    [SerializeField] private string shopSceneName;
+
+    public void Load()
+    {
+        SceneManager.LoadScene(shopSceneName, LoadSceneMode.Additive);
+    }
+    
+    public void Unload()
+    {
+        SceneManager.UnloadSceneAsync(shopSceneName);
+        var lobbyScreen = FindObjectOfType<LobbyScreen>();
+        if(lobbyScreen != null)
+            lobbyScreen.SetReadyViaShop(lobbyScreen.oldReadyState);
+    }
+}
